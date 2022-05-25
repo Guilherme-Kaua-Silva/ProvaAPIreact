43cfd48c22c2eb429bf64d83fd05fa8feb868e4c
Guilherme <pepeks.rx.1@gmail.com>
2022-05-25 18:40:45 -0300

    okok

--- src/siteRoutes.js
+++ src/siteRoutes.js
@@ -1,11 +1,11 @@
 
 import { BrowserRouter , Routes , Route} from 'react-router-dom'
 
-import { Home } from '/pages/home'
+import { Home } from './pages/home'
 import { CorPrimaria } from './pages/corprimaria'
-import { Frequencia } from '/pages/frequencia'
-import { Ingresso } from '/pages/ingresso'
-import { MaiorNumero } from '/pages/maiornumero'
+import { Frequencia } from './pages/frequencia'
+import { Ingresso } from './pages/ingresso'
+import { MaiorNumero } from './pages/maiornumero'
 
 export default function SiteRoutes(){
     return(
@@ -13,7 +13,7 @@ export default function SiteRoutes(){
         
             <Routes>
 
-                <Route path= '/home' element={<Home />} />
+                <Route path= '/' element={<Home />} />
                 <Route path= '/corprimaria' element={<CorPrimaria />} />
                 <Route path= '/frequencia' element={<Frequencia />} />
                 <Route path= '/ingresso' element={<Ingresso />} />
